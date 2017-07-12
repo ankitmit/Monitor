0d289ca077b08328b0dc0ba86373489886f613fb
Ankit Mittal <ankitmittal@yahoo-inc.com>
2017-07-12 11:46:24 -0700

    Adding the logger script

---
+++ logger.py
@@ -0,0 +1,24 @@
+import requests
+import os
+import json
+import time
+import smtplib
+import dropbox
+import re
+import datetime
+import logging
+import pytz
+
+def getFileFromDropBox():
+    data = ''
+    auth_token = 'e7pGKdpUoZAAAAAAAAABcX28r0cul9w9slEQyuaAutv88eJc7Qz0ta8wxak2bguO'
+    dbx = dropbox.Dropbox(auth_token)
+    dbx.users_get_current_account()
+    path = '/Finance/records.txt'
+    try:
+        md, res = dbx.files_download(path)
+    except dropbox.exceptions.HttpError as err:
+        logger.info('*** HTTP error', err)
+        return None
+    data = res.content
+    print data
