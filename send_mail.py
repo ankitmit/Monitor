adb8094e088763b9d692426e249d82007151cac0
Ankit Mittal <coolraj.india@gmail.com>
2018-10-09 12:44:34 -0700

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -1,13 +1,6 @@
 import smtplib
-
-class SendMailServer:
-  def __init__(self, port, provider):
-    self.port = port
-    self.provider = provider
-
-    self.server = smtplib.SMTP_SSL(provider, port)
-    self.server.login('crackcat2k11', 'ankitmittalbgh')
-
-    
-  def SendMail(self, email_text):
-    self.server.sendmail('crackcat2k11@gmail.com', 'crackcat2k11@gmail.com', email_text)
+ 
+def SendMail(self, email_text, port, provider):
+  server = smtplib.SMTP_SSL(provider, port)
+  server.login('crackcat2k11', 'ankitmittalbgh')
+  server.sendmail('crackcat2k11@gmail.com', 'crackcat2k11@gmail.com', email_text)
